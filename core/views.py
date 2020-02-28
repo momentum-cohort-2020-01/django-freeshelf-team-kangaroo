a69ed09e886e4367ab085983d4fd48da883f7047
gabishop35 <60151915+gabishop35@users.noreply.github.com>
2020-02-28 16:25:31 -0500

    Update views.py

--- core/views.py
+++ core/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpRequest
 from .models import Book
 from .forms import BookForm
-from taggit.models import Tag
 
 
 # Create your views here.
